300db4807e4e25af57e3a366e5087fe237cf954b
Oleg Konovalov <oleg.konovalov@happytravel.com>
2021-01-29 02:00:25 +0300

    Add access token support for the Accommodation mapper client into the LocationService

--- HappyTravel.LocationService/Infrastructure/Extensions/HttpClientsCollectionExtension.cs
+++ HappyTravel.LocationService/Infrastructure/Extensions/HttpClientsCollectionExtension.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using HappyTravel.LocationService.Services.HttpClients;
 using HappyTravel.VaultClient;
+using IdentityModel.Client;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,9 +11,28 @@ namespace HappyTravel.LocationService.Infrastructure.Extensions
     {
         public static IServiceCollection AddHttpClients(this IServiceCollection services, IConfiguration configuration, IVaultClient vaultClient)
         {
-            var options = vaultClient.Get(configuration["Elasticsearch:Mapper"]).GetAwaiter().GetResult();
-            services.AddHttpClient<MapperHttpClient>(client => client.BaseAddress = new Uri(options["endpoint"]));
-
+            var mapperClientOptions = vaultClient.Get(configuration["Elasticsearch:Mapper"]).GetAwaiter().GetResult();
+            var authorityOptions = vaultClient.Get(configuration["Authority:Options"]).GetAwaiter().GetResult();
+           
+            services.AddAccessTokenManagement(options =>
+            {
+                options.Client.Clients.Add(HttpClientNames.MapperIdentityClient, new ClientCredentialsTokenRequest
+                {
+                    Address = $"{authorityOptions["authorityUrl"]}connect/token",
+                    ClientId = mapperClientOptions["clientId"],
+                    ClientSecret = mapperClientOptions["clientSecret"],
+                    Scope = mapperClientOptions["scope"]
+                });
+            });
+            services.AddClientAccessTokenClient(HttpClientNames.MapperApi, HttpClientNames.MapperIdentityClient, 
+                client =>
+            {
+                client.BaseAddress = new Uri(mapperClientOptions["endpoint"]);
+            });
+            
+            services.AddHttpClient(HttpClientNames.MapperApi);
+            services.AddTransient<IAccommodationMapperHttpClient, AccommodationMapperHttpClient>();
+            
             return services;
         }
     }
--- HappyTravel.LocationService/Services/Locations/Mapper/MapperLocationsManagementService.cs
+++ HappyTravel.LocationService/Services/Locations/Mapper/MapperLocationsManagementService.cs
@@ -19,12 +19,12 @@ namespace HappyTravel.LocationService.Services.Locations.Mapper
 {
     public class MapperLocationsManagementService : IMapperLocationsManagementService
     {
-        public MapperLocationsManagementService(IElasticClient elasticClient, MapperHttpClient mapperHttpClient, IOptions<IndexOptions> indexOptions, ILogger<IMapperLocationsManagementService> logger)
+        public MapperLocationsManagementService(IElasticClient elasticClient, IAccommodationMapperHttpClient accommodationMapperHttpClient, IOptions<IndexOptions> indexOptions, ILogger<IMapperLocationsManagementService> logger)
         {
             _elasticClient = elasticClient;
             _logger = logger;
             _indexOptions = indexOptions.Value;
-            _mapperHttpClient = mapperHttpClient;
+            _accommodationMapperHttpClient = accommodationMapperHttpClient;
         }
 
         
@@ -69,7 +69,7 @@ namespace HappyTravel.LocationService.Services.Locations.Mapper
                 cancellationToken.ThrowIfCancellationRequested();
                 bool isFailure;
                 string error;
-                (_, isFailure, locations, error) = await _mapperHttpClient.GetLocations(locationType, languageCode, default, skip, batchSize, cancellationToken);
+                (_, isFailure, locations, error) = await _accommodationMapperHttpClient.GetLocations(locationType, languageCode, default, skip, batchSize, cancellationToken);
                 if (isFailure)
                     yield return Result.Failure<List<Location>>(error);
                 
@@ -144,7 +144,7 @@ namespace HappyTravel.LocationService.Services.Locations.Mapper
         }
         
 
-        private readonly MapperHttpClient _mapperHttpClient;
+        private readonly IAccommodationMapperHttpClient _accommodationMapperHttpClient;
         private readonly IElasticClient _elasticClient;
         private readonly IndexOptions _indexOptions;
         private readonly ILogger<IMapperLocationsManagementService> _logger;
